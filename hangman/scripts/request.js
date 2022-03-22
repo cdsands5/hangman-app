342c54d58897064cae3b3fb40936abbbba7c2e35
cdsands5 <94807540+cdsands5@users.noreply.github.com>
2022-03-22 14:26:32 -0400

    Update request.js

--- hangman/scripts/request.js
+++ hangman/scripts/request.js
@@ -9,32 +9,8 @@ const getPuzzle = async (wordCount) => {
     }
 }
 
-const getCurrentCountry = async () => {
-    const location = await getLocation()
-    return  getCountry(location.country)
-    
-}
 
-const getCountry =  async (countryCode) => {
-    const response = await fetch('//api.countrylayer.com/v2/all?access_key=765d240f256077f9adba5301005c184b')
 
-    if(response.status === 200) {
-        const data = await response.json()
-        return data.find((country) => country.alpha2Code === countryCode)
-    }else {
-        throw new Error('Unable to fetch data')
-    }
-}
-
-const getLocation = async () => {
-    const response = await fetch('//ipinfo.io/json?6803cf50f6303b')
-    
-    if(response.status === 200) {
-        return response.json()
-    }else {
-        throw new Error('Unable to fetch location')
-    }
-}
 
 
 
